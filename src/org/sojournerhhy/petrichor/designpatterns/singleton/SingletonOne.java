f402edda35f116906488868e6d9513360e3c8d2e
hanhongying <hanhongying@corp.netease.com>
2019-03-06 17:28:33 +0800

    @dev 单例模式

--- src/org/sojournerhhy/petrichor/designpatterns/singleton/SingletonOne.java
+++ src/org/sojournerhhy/petrichor/designpatterns/singleton/SingletonOne.java
@@ -1,11 +1,21 @@
 package org.sojournerhhy.petrichor.designpatterns.singleton;
 
 /**
- * Description:
+ * Description: 饿汉模式，类加载时完成初始化
  *
  * @author hanhongying@corp.netease.com
  * @version 2019/3/6
  */
 public class SingletonOne {
 
+
+    private static SingletonOne instance = new SingletonOne();
+
+    private SingletonOne() {
+
+    }
+
+    public static SingletonOne getInstance() {
+        return instance;
+    }
 }
